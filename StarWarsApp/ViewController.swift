bdae6bc46c90aa72e87639e475d5db4555be32b4
MattHuie <matthuie93@gmail.com>
2019-08-11 16:45:49 -0400

    first commit

---
+++ StarWarsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarWarsApp
+//
+//  Created by Matthew Huie on 8/11/19.
+//  Copyright © 2019 Matthew Huie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
